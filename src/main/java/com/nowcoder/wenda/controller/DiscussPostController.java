50ccee7acdb18cd4ecd86c0aaea29483b40e5455
10471 <1047106689@qq.com>
2021-05-12 18:00:31 +0800

    v2第二版 2.1.15帖子详情

--- src/main/java/com/nowcoder/wenda/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/wenda/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.nowcoder.wenda.controller;
 import com.nowcoder.wenda.entity.DiscussPost;
 import com.nowcoder.wenda.entity.User;
 import com.nowcoder.wenda.service.DiscussPostService;
+import com.nowcoder.wenda.service.UserService;
 import com.nowcoder.wenda.util.WendaUtil;
 import com.nowcoder.wenda.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +25,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -40,6 +46,18 @@ public class DiscussPostController {
 
         // 报错的情况,将来统一处理.
         return WendaUtil.getJSONString(0, "发布成功!");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        // 作者
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
     }
 
 }
