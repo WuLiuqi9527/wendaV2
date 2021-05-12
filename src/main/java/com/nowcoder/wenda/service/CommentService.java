98b066cd58c271f6028d4efb6ac310b0985e3e8e
10471 <1047106689@qq.com>
2021-05-12 20:59:12 +0800

    v2第二版 2.1.17显示评论

---
+++ src/main/java/com/nowcoder/wenda/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.nowcoder.wenda.service;
+
+import com.nowcoder.wenda.dao.CommentMapper;
+import com.nowcoder.wenda.entity.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
+        return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
+    }
+
+    public int findCommentCount(int entityType, int entityId) {
+        return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+}
