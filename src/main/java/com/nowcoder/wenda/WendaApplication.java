007eae6cdebae872cfd6eb38c2da151dac117639
10471 <1047106689@qq.com>
2021-05-15 22:46:44 +0800

    v2第二版 2.4.1Spring整合Elasticsearch

--- src/main/java/com/nowcoder/wenda/WendaApplication.java
+++ src/main/java/com/nowcoder/wenda/WendaApplication.java
@@ -3,8 +3,17 @@ package com.nowcoder.wenda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class WendaApplication {
+
+	@PostConstruct
+	public void init() {
+		// 解决netty启动冲突问题
+		// see Netty4Utils.setAvailableProcessors()
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WendaApplication.class, args);
